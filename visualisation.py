73ed0d13d00f0b3e1a0426365de9ba24a52fb330
Sebastian Law <sebastianhlaw@gmail.com>
2016-11-25 17:28:31 +0000

    clean up

--- visualisation.py
+++ visualisation.py
@@ -7,8 +7,9 @@ from matplotlib.pylab import plt
 
 
 def plot(Xi, yi=None):
+    plt.close('all')
     side = np.sqrt(Xi.size).astype(int)
     data = np.array(Xi).reshape((side, side))
     plt.imshow(data, cmap=plt.cm.gray_r, interpolation='nearest')
     plt.title("y = "+str(yi))
-    # plt.show()
+    plt.show()
