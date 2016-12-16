464cd90ac791f96b3f962fde660a5b95a2876944
Sebastian Law <sebastianhlaw@gmail.com>
2016-12-16 17:09:34 +0000

    Refactoring: plotting pca nad lda factors now in visualisation.py

--- factor_analysis_dev.py
+++ factor_analysis_dev.py
@@ -2,35 +2,25 @@
 # Date:     25-Nov-2016
 # Revised:  15-Dec-2016
 
-from sklearn import preprocessing
+from sklearn import preprocessing, model_selection
 from sklearn.decomposition import PCA, FactorAnalysis
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import matplotlib.pyplot as plt
 
 import loader
 import factor_analysis
+X_data, y_data = loader.load_sklearn()
+# X_data, y_data = loader.load_kaggle_public()
 
-X, y = loader.load_sklearn()
+# X_train, X_validate, y_train, y_validate = model_selection.train_test_split(
+#     X_data, y_data, train_size=0.25, test_size=0.05, random_state=0)
 
-le = preprocessing.LabelEncoder()
-le.fit(y)
-labels = ["y=" + str(i) for i in le.classes_]
-colours = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'darkgreen', 'darkorange', 'yellow', 'black']
 
-pca = PCA(n_components=2)
-X_pca = pca.fit(X).transform(X)  # also fit_transform(X) not quite sure the difference
-X_inv = pca.inverse_transform(X_pca)
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_lda = lda.fit(X_data, y_data).transform(X_data)
 plt.figure()
 for i, j in enumerate(le.classes_):
-    plt.scatter(X_pca[y == j, 0], X_pca[y == j, 1], alpha=0.8, color=colours[i], label=str(j))
+    plt.scatter(X_lda[y_data == j, 0], X_lda[y_data == j, 1], alpha=0.8, color=colours[i], label=str(j))
 plt.legend(loc='best', shadow=False, scatterpoints=1)
-plt.title('PCA')
-
-# lda = LinearDiscriminantAnalysis(n_components=2)
-# X_lda = lda.fit(X, y).transform(X)
-# plt.figure()
-# for i, j in enumerate(le.classes_):
-#     plt.scatter(X_lda[y == j, 0], X_lda[y == j, 1], alpha=0.8, color=colours[i], label=str(j))
-# plt.legend(loc='best', shadow=False, scatterpoints=1)
-# plt.title('LDA')
+plt.title('LDA')
 
--- visualisation.py
+++ visualisation.py
@@ -1,15 +1,38 @@
 # Author:   Sebastian Law
 # Date:     22-Nov-2016
-# Revised:  22-Nov-2016
+# Revised:  16-Dec-2016
 
 import numpy as np
 from matplotlib.pylab import plt
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
-def plot(Xi, yi=None):
+def display_digit(Xi, yi=None):
     # plt.close('all')
     side = np.sqrt(Xi.size).astype(int)
     data = np.array(Xi).reshape((side, side))
     plt.imshow(data, cmap=plt.cm.gray_r, interpolation='nearest')
     plt.title("y = "+str(yi))
     plt.show()
+
+
+def plot_first_factors(X_data, y_data, type):
+    le = preprocessing.LabelEncoder()
+    le.fit(y_data)
+    colours = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'darkgreen', 'darkorange', 'yellow', 'black']
+    if type == "pca":
+        transform = PCA(n_components=2)
+        XX_data = transform.fit(X_data).transform(X_data)
+    elif type == "lda":
+        transform = LinearDiscriminantAnalysis(n_components=2)
+        XX_data = transform.fit(X_data, y_data).transform(X_data)
+    else:
+        print("Type", type, "not recognised, use either 'pca' or 'lda'.")
+        return
+    plt.figure()
+    for i, j in enumerate(le.classes_):
+        plt.scatter(XX_data[y_data == j, 0], XX_data[y_data == j, 1], alpha=0.8, color=colours[i], label=str(j))
+    plt.legend(loc='best', shadow=False, scatterpoints=1)
+    plt.title(type)
